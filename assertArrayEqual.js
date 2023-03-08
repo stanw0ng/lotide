aedd7967ec108643053e631ef9380fe56f8d7275
stanw0ng <user.stanley@gmail.com>
2023-03-07 19:36:50 -0500

    added assertArrayEqual.js

---
+++ assertArrayEqual.js
@@ -0,0 +1,20 @@
+const assertArraysEqual = function(array1, array2) {
+  let message = "";
+
+  if (array1.length !== array2.length) {
+    message += `👎 Assertion Failed: the arrays are not the same `;
+    return message;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      message += `👎 Assertion Failed: the arrays are not the same`;
+      return message;
+    }
+  };
+
+  message += `👍 Assertion Passed: the arrays are equal`;
+  return message;
+};
+
+console.log(assertArraysEqual(array3, array4));
