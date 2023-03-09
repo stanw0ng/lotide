59e6cc19a71f06d976b698837a6b1f2f505cc91c
stanw0ng <user.stanley@gmail.com>
2023-03-09 16:54:59 -0500

    added findKey.js

---
+++ findKey.js
@@ -0,0 +1,37 @@
+//assertEqual
+const assertEqual = function(actual, expected) {
+  let message = '';
+
+  if (actual === expected) {
+    message += `👍 Assertion Passed: ${actual} === ${expected}`;
+    return message;
+  }
+  
+  if (actual !== expected) {
+    message += `👎 Assertion Failed: ${actual} !== ${expected}`;
+    return message;
+  }
+
+  return message;
+};
+
+//findKey
+const findKey = (obj, callback) => {
+  for (let key in obj) {
+    if (callback(obj[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+const result = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2);
+
+console.log(assertEqual(result, 'noma'));
