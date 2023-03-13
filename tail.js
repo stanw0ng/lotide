29b8eafca824a861a254a8b4cec6a151498e0ab6
stanw0ng <user.stanley@gmail.com>
2023-03-13 10:27:40 -0400

    refactored tail.js and added test file

--- tail.js
+++ tail.js
@@ -1,13 +1,6 @@
-// assertEqual to check
-const assertEqual = function(actual, expected) {
+// assertEqual for testing
 
-  if (actual !== expected) {
-    console.log(`👎 Assertion Failed: ${actual} !== ${expected}`);
-    return;
-  }
-
-  console.log(`👍 Assertion Passed: ${actual} === ${expected}`);
-};
+const assertEqual = require('./assertEqual');
 
 // tail function
 
@@ -22,19 +15,4 @@ const tail = function(array) {
   return array.slice(1);
 };
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-// Test Case 2: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-// Test Case 3: Checking single word array output
-const word = ["Hey"];
-const emptyArray = tail(word);
-console.log(emptyArray);
-assertEqual(emptyArray.length, word.length);
+module.exports = tail;
