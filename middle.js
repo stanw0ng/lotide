b68dc6e2951ad98a119d4950259ed8b08eff93b0
stanw0ng <user.stanley@gmail.com>
2023-03-13 14:32:03 -0400

    modified and refactored middle.js

--- middle.js
+++ middle.js
@@ -1,25 +1,10 @@
-//eqArray base logic
-
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-//assertArraysEqual function
-const assertArraysEqual = require('./assertArrayEqual')
-
 // middle function
 const middle = function(array) {
   const result = [];
+
+  if(array.length === 1) {
+    return result
+  }
   
   if(array.length % 2 !== 0) {
     const midIndex = ((array.length-1)/2);
--- test/middleTest.js
+++ test/middleTest.js
@@ -6,12 +6,19 @@ const middle = require("../middle");
 let result = middle([1, 2, 3]);
 console.log(result);
 assertArraysEqual(result, [2]);
+
 result = middle([1, 2, 3, true, 5, 6, 7]);
 console.log(result);
 assertArraysEqual(result, [true]);
+
+result = middle([1]);
+console.log(result);
+assertArraysEqual(result, []);
+
 result = middle([1, 2, 3, true, 5, 6]);
 console.log(result);
 assertArraysEqual(result, []);
+
 result = middle([]);
 console.log(result);
 assertArraysEqual(result, []);
